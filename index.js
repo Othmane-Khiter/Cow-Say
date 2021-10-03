3d31ea06fda9a44eeb2fbf8e7fc975266e628f02
Othmane Khiter <othmane.khiter@gmail.com>
2021-10-03 13:54:16 +0200

    Created nodejs files to have a cow who say my informations with the module cowsay

---
+++ index.js
@@ -0,0 +1,8 @@
+const userInfo = require('./information');
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+text: `Hello I'm ${userInfo.firstName} ${userInfo.lastName} from ${userInfo.campus} Campus !`,
+e:"oO",
+T:"U"
+}))
---
+++ information.js
@@ -0,0 +1,11 @@
+const userInfo = {
+    firstName: "Othmane",
+    lastName: "Khiter",
+    campus: "Lille",
+}
+
+module.exports = {
+    firstName: userInfo.firstName,
+    lastName: userInfo.lastName,
+    campus: userInfo.campus,
+}
